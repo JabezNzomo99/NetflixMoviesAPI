6c9d342c94f1e432c16dee0b6a38ab75b24b1c38
Jabez <jaybezdon@gmail.com>
2019-10-31 18:19:21 +0300

    Server changes

--- src/main/java/com/project/netflixapi/feigntest/FeignRestClientTest.java
+++ src/main/java/com/project/netflixapi/feigntest/FeignRestClientTest.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
 public class FeignRestClientTest implements CommandLineRunner {
 
     private final FeignRestClient feignRestClient;
